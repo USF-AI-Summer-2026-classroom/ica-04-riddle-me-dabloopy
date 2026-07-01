ea47954357c1e9341964bf205ab5b4c107abd494
Brian Lopez <lopezrocha3531@gmail.com>
2026-06-30 23:16:23 -0700

    [V02] Added formulas and explenations for the crimes and updated the argument forms

--- riddleme.py
+++ riddleme.py
@@ -11,28 +11,32 @@ from logic import *
 #T = Tell-tale clue
 #S = Street criminal
 #Q = Riddle clues (word games, etc.)
+# H = A hole was found
 
-J, P, R, A, C, B, U, T, S, Q = vars('J', 'P', 'R', 'A', 'C', 'B', 'U', 'T', 'S', 'Q')
+J, P, R, A, C, B, U, T, S, Q, H = vars('J', 'P', 'R', 'A', 'C', 'B', 'U', 'T', 'S', 'Q', 'H')
 
 # Formulas
 
 f01 = T # A tell-tale clue was found
 f02 = T >> ~S # If a tell-tale clue is found, it is not a street criminal
 f03 = T >> (J | R | P) # If a tell-tale clue is found, it's a supervillain
-f04 = R >> Q # Riddler leaves riddles
-f05 = P >> U # Penguin leaves umbrella marks
-f06 = J >> (A | B | C) # Joker leaves acid, buzzers, or cards
+f04 = R << Q # Riddler is guilty if and only if there are riddle clues
+f05 = P << U # Penguin is guilty if and only if there is an umbrella mark
+f06 = J << (A | B | C) # Joker is guilty if and only if there is acid, buzzer, or cards
+f07 = H     # Batman found the hole!
+f08 = H >> (A | U) # The hole is from acid or umbrella
 
 
 # ArgumentForms
 # jokers argument form
-joker_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, conclusion=J)
+joker_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, f07, f08, conclusion=J)
 # penguins argument form
-penguin_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, conclusion=P)
+penguin_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, f07, f08, conclusion=P)
 # riddlers argument form
-riddler_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, conclusion=R)
+riddler_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, f07, f08, conclusion=R)
 # street criminals argument form
-street_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, conclusion=S)
+street_arg = ArgumentForm(f01, f02, f03, f04, f05, f06, f07, f08, conclusion=S)
+
 
 #Evaluate the argument forms
 print("Who definitely committed this crime:")
